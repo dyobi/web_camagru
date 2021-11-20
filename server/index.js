d849c91b8430b940325e049416f0b9d4c310937e
Dyobi <dyobi@Dyobi.local>
2021-11-20 19:40:28 +0900

    .

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ const conn = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'gusdk314',
+    password: '**********',
     database: 'camagru'
 })
 
